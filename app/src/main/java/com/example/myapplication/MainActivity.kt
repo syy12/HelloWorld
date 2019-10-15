bf64009cef50e3931836a9d1c2e174573526d72f
syy12 <szeyy-wm17@student.tarc.edu.my>
2019-10-15 15:54:20 +0800

    initial

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() { //: means extend, AppCompatActivity is a parent class
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //onCreate = main()
+        super.onCreate(savedInstanceState)
+
+        //create the UI
+        //R = resources
+        setContentView(R.layout.activity_main)
+        //TODO Continue your work here
+        //val = value, var = variable
+        val buttonHelloWorld : Button = findViewById(R.id.buttonHelloWorld);
+        buttonHelloWorld.setOnClickListener { showMessage() }
+
+        val buttonReset : Button = findViewById(R.id.buttonReset);
+        buttonReset.setOnClickListener { resetMessage() }
+
+    }
+
+    private fun resetMessage() {
+        val textViewMessage : TextView = findViewById(R.id.textViewMessage);
+        //textViewMessage.setText(getString(R.id.textViewMessage));
+        textViewMessage.setText("Hello world");
+    }
+
+    private fun showMessage() {
+        val textViewMessage : TextView = findViewById(R.id.textViewMessage);
+        textViewMessage.setText("Hello <your name here>");
+    }
+}
